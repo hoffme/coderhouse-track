513596a6f0b76e99a7157f498815d1cf61ed01fe
hoffme <phjocoronel@gmail.com>
2021-01-27 18:41:38 -0300

    agregando componente itemListContainer

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 
 import NavBar from './components/navBar';
 import CartWidget from './components/cartWidget';
+import ItemListContainer from './components/itemListContainer';
 
 function App() {
   return (
     <>
       <NavBar/>
+      <ItemListContainer/>
       <CartWidget/>
     </>
   );
---
+++ src/components/itemListContainer/index.jsx
@@ -0,0 +1,9 @@
+import './style.css';
+
+const ItemListContainer = () => {
+    return <div className={"item-list-container app-width"}>
+        <label>No se han encontrado articulos</label>
+    </div>;
+}
+
+export default ItemListContainer;
