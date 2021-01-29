cb75f02e3a1bc5df950dcde1ccc3af4be11db522
hoffme <phjocoronel@gmail.com>
2021-01-29 12:36:38 -0300

    refactor navbar scroll

--- src/components/navBar/index.jsx
+++ src/components/navBar/index.jsx
@@ -13,6 +13,18 @@ const NavBar = () => {
                 <li><a href={"/cervezas"}>Cerverzas</a></li>
                 <li><a href={"/vinos"}>Vinos</a></li>
                 <li><a href={"/sin-alcohol"}>Sin Alcohol</a></li>
+                <li><a href={"/combos"}>Combos</a></li>
+                <li><a href={"/cervezas"}>Cerverzas</a></li>
+                <li><a href={"/vinos"}>Vinos</a></li>
+                <li><a href={"/sin-alcohol"}>Sin Alcohol</a></li>
+                <li><a href={"/combos"}>Combos</a></li>
+                <li><a href={"/cervezas"}>Cerverzas</a></li>
+                <li><a href={"/vinos"}>Vinos</a></li>
+                <li><a href={"/sin-alcohol"}>Sin Alcohol</a></li>
+                <li><a href={"/combos"}>Combos</a></li>
+                <li><a href={"/cervezas"}>Cerverzas</a></li>
+                <li><a href={"/vinos"}>Vinos</a></li>
+                <li><a href={"/sin-alcohol"}>Sin Alcohol</a></li>
             </ul>
         </nav>
     </header>;
