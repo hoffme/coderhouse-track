8395c2fa19b857d140040a78c17b7eb0b4429864
Juan Coronel <phjocoronel@gmail.com>
2021-02-05 21:05:10 -0300

    refactor style galery

--- src/components/galery/index.js
+++ src/components/galery/index.js
@@ -6,17 +6,37 @@ const Galery = ({images = []}) => {
     const [imgShowing, setImgShowing] = useState(images[0]);
 
     return <div className={"galery"}>
+        <div className={"showing"}>
+            <button
+                className={"button-back"}
+                onClick={() => {
+                    const actualIndex = images.indexOf(imgShowing);
+                    setImgShowing(images[actualIndex - 1]);
+                }}
+                disabled={images.indexOf(imgShowing) === 0}
+            >{"<"}</button>
+            <button
+                className={"button-next"}
+                onClick={() => {
+                    const actualIndex = images.indexOf(imgShowing);
+                    setImgShowing(images[actualIndex + 1]);
+                }}
+                disabled={images.indexOf(imgShowing) === (images.length - 1)}
+            >{">"}</button>
+            <img src={imgShowing.url} alt={imgShowing.alt} />
+        </div>
         <div className={"preview"}>
             {
-                images.map(({url, alt}, index) => {
-                    return <button key={index} onClick={() => setImgShowing({url, alt})}>
-                        <img src={url} alt={alt} />
+                images.map((img, index) => {
+                    return <button
+                        className={(img === imgShowing) ? 'selected' : null}
+                        key={index}
+                        onClick={() => setImgShowing(img)}
+                    >
+                        <img src={img.url} alt={img.alt} />
                     </button>
                 })
             }
-        </div>
-        <div className={"showing"}>
-            <img src={imgShowing.url} alt={imgShowing.alt} />
         </div>
     </div>;
 }
