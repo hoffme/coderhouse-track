4b7c0dc062bf3807866c984654ab27e9ed607820
Juan Coronel <phjocoronel@gmail.com>
2021-03-03 10:38:54 -0300

    refactor comprar Ya link

--- src/components/product/control/index.jsx
+++ src/components/product/control/index.jsx
@@ -41,7 +41,7 @@ const ProductControl = ({product}) => {
             className={"button-buy-now"}
             onClick={() => {
                 addItem(product, count);
-                history.push('/cart/');
+                history.push('/checkout/');
             }}
         >Comprar Ya</button>
     </>;
