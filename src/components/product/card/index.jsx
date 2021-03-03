5475d5f07db1d86bcdba18048a7ca384d9084859
Juan Coronel <phjocoronel@gmail.com>
2021-03-02 22:31:58 -0300

    refactor product cart

--- src/components/product/card/index.jsx
+++ src/components/product/card/index.jsx
@@ -5,7 +5,6 @@ const ProductCard = ({ product }) => {
     return <Link className={"product-card"} to={'/product/' + product.url}>
         <img src={ product.picture.url } alt={ product.picture.alt } />
         <b className={"title"}>{ product.title }</b>
-        <label className={"description"}>{product.description}</label>
         <b className={"price"}>{ "$ " + product.price }</b>
     </Link>;
 }
