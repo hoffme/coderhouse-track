d024d9bb84f0858740fdb7553db5b03207d14339
Juan Coronel <phjocoronel@gmail.com>
2021-03-03 07:33:01 -0300

    fix redirect to login

--- src/pages/user/index.jsx
+++ src/pages/user/index.jsx
@@ -1,6 +1,6 @@
 import './style.scss';
 
-import { Redirect, Route, Switch, useHistory, useLocation} from 'react-router-dom';
+import { Redirect, Route, Switch} from 'react-router-dom';
 
 import Header from '../../components/header';
 
@@ -9,17 +9,10 @@ import UserContext from '../../contexts/user';
 
 const UserPage = ({match}) => {
     const { usuario } = useContext(UserContext);
-    const location = useLocation();
-    const history = useHistory();
 
     return <>
         <Switch>
-            <Redirect exact path={match.url} to={match.url + "info/"}></Redirect>
-            <Route path={match.url + "/info"} >
-                <Header />
-                <h1>Info Page</h1>
-                { (!usuario && location.pathname === '/user/info/') && history.push('/user/login/') }
-            </Route>
+            <Redirect exact path={match.url} to={match.url + "info"}></Redirect>
             <Route path={match.url + "/login"} >
                 <h1>login Page</h1>
             </Route>
@@ -29,6 +22,14 @@ const UserPage = ({match}) => {
             <Route path={match.url + "/restore"} >
                 <h1>restore Page</h1>
             </Route>
+            <Route path={match.url + "/info"} >{
+                usuario ?
+                    <>
+                        <Header />
+                        <h1>Info Page</h1>
+                    </> :
+                    <Redirect to={match.url + "/login"} />
+            }</Route>
         </Switch>
     </>;
 }
