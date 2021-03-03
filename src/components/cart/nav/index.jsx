26d3537fad0ab7eb330300321eecd4636c239cf2
Juan Coronel <phjocoronel@gmail.com>
2021-03-03 10:25:27 -0300

    refactor navCart

--- src/components/cart/nav/index.jsx
+++ src/components/cart/nav/index.jsx
@@ -1,19 +1,31 @@
 import { useState } from 'react';
+
 import './style.scss';
 
-const NavCart = () => {
+const NavCart = ({onNext = () => null, onBack = () => null}) => {
     const [message, setMessage] = useState(null);
 
-    const showMessage = (message) => {
-        setMessage(message);
-        setTimeout(() => setMessage(null), 5000);
+    const showMessage = text => {
+        setMessage(text);
+        
+        new Promise(resolve => {
+            const t = setTimeout(() => {
+                setMessage(null);
+                resolve(t);
+            }, 5000)
+        }).then(t => clearTimeout(t));
+    }
+
+    const onClick = () => {
+        const message = onNext();
+        if (message && message.length > 0) showMessage(message)
     }
 
     return <div className={"nav-cart"}>
         { (message && message.length > 0) && <div className={"message"}>{message}</div> }
         <div className={"controls"}>
-            <button className={"volver"}>Volver</button>
-            <button className={"siguiente"} onClick={() => showMessage("hola que tal")}>Sigueinte</button>
+            <button className={"volver"} onClick={onBack}>Volver</button>
+            <button className={"siguiente"} onClick={onClick}>Sigueinte</button>
         </div>
     </div>
 }
