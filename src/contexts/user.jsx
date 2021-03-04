395ef81678c8a2d376c3f6b71ce8f6abc9a04f26
Juan Coronel <phjocoronel@gmail.com>
2021-03-04 09:01:25 -0300

    user get address

--- src/contexts/user.jsx
+++ src/contexts/user.jsx
@@ -1,18 +1,35 @@
 import { useEffect, useState, createContext } from "react";
-import { getAuth } from '../firebase';
+import { getAuth, getFirestore } from '../firebase';
 
 const UserContext = createContext();
 
 const UserProvider = ({ children }) => {
     const [user, setUser] = useState(null);
+    const [address, setAddress] = useState(null);
 
     useEffect(() => {
-        getAuth().onAuthStateChanged(user => setUser(user));
+        getAuth().onAuthStateChanged(user => {
+            setUser(user);
+
+            getFirestore()
+                .collection('address')
+                .where("user_id", "==", user.uid)
+                .get()
+                .then(snapshot => {
+                    setAddress(snapshot.docs.map(doc => {
+                        return { id: doc.id, ...doc.data() };
+                    }));
+                })
+                .catch(() => setAddress([]));
+        });
 
         return () => {}
     }, []);
 
-    return <UserContext.Provider value={{user}}>
+    return <UserContext.Provider value={{
+        user,
+        address
+    }}>
         {children}
     </UserContext.Provider>
 };
