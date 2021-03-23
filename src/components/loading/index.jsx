c0b575ef0dc68fa40ef41c06441edcb6ba5a7f26
Juan Coronel <phjocoronel@gmail.com>
2021-03-23 11:08:18 -0300

    alt loading gif

--- src/components/loading/index.jsx
+++ src/components/loading/index.jsx
@@ -3,7 +3,7 @@ import './style.scss';
 import icon from '../../assets/icons/loading.gif';
 
 const Loading = () => {
-    return <div className={"loading"}> <img src={icon} /> </div>
+    return <div className={"loading"}> <img alt={"loading gif"} src={icon} /> </div>
 }
 
 export default Loading;
